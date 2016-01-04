3f26db9ccef5cf630c739cfcfcd96a3d4a16d568
Eric Carty-Fickes <ericcf@northwestern.edu>
2016-01-04 15:22:25 -0600

    add resource type for non-syncing data

---
+++ src/LocalResource.js
@@ -0,0 +1,64 @@
+(function(context) {
+  'use strict';
+
+  var LocalResource = {
+    storeType: lf.schema.DataStoreType.INDEXED_DB,
+
+    setStoreType: function setStoreType(type) {
+      this.storeType = type;
+
+      return this;
+    },
+
+    setSchemaBuilder: function setSchemaBuilder(schemaBuilder) {
+      this.schemaBuilder = schemaBuilder;
+
+      return this;
+    },
+
+    setTableName: function setTableName(name) {
+      this.tableName = name;
+
+      return this;
+    },
+
+    connectToDb: function connectToDb() {
+      return this.schemaBuilder.connect({
+        storeType: this.storeType
+      });
+    },
+
+    getTable: function getTable(db) {
+      return db.getSchema().table(this.tableName);
+    },
+
+    createTable: function createTable() {
+      var isAutoIncrementing = true;
+
+      this.schemaBuilder
+        .createTable(this.tableName)
+        .addColumn('id', lf.Type.INTEGER)
+        .addPrimaryKey(['id'], isAutoIncrementing);
+
+      return this;
+    },
+
+    fetchAll: function fetchAll(connection) {
+      return connection.then((function(db) {
+        return db.select().from(this.getTable(db)).exec();
+      }).bind(this));
+    },
+
+    persist: function persist(connection, record) {
+      return connection.then((function(db) {
+        var table = this.getTable(db);
+        var row = table.createRow(record);
+
+        return db.insert().into(table).values([row]).exec();
+      }).bind(this));
+    }
+  };
+
+  context.cbit = context.cbit || {};
+  context.cbit.LocalResource = LocalResource;
+})(this);
---
+++ test/LocalResourceSpec.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var LocalResource = cbit.LocalResource;
+
+describe('LocalResource', function() {
+  var DB_NAME = 'app_test';
+
+  function getSchemaBuilder() {
+    return lf.schema.create(DB_NAME, 1);
+  }
+
+  describe('#createTable', function() {
+    describe('when no schemaBuilder property is assigned', function() {
+      it('throws an exception', function() {
+        var cache = Object.create(LocalResource)
+          .setStoreType(lf.schema.DataStoreType.MEMORY);
+
+        expect(cache.createTable.bind(cache)).toThrow();
+      });
+    });
+
+    describe('when a schemaBuilder property is assigned', function() {
+      describe('when no tableName property is assigned', function() {
+        it('throws an exception', function() {
+          var cache = Object.create(LocalResource)
+            .setSchemaBuilder(getSchemaBuilder())
+            .setStoreType(lf.schema.DataStoreType.MEMORY);
+
+          expect(cache.createTable.bind(cache)).toThrow();
+        });
+      });
+
+      describe('when a tableName property is assigned', function() {
+        it('adds an IndexedDB table', function() {
+          var cache = Object.create(LocalResource)
+            .setSchemaBuilder(getSchemaBuilder())
+            .setTableName('my_table')
+            .setStoreType(lf.schema.DataStoreType.MEMORY);
+
+          expect(cache.createTable.bind(cache)).not.toThrow();
+        });
+      });
+    });
+  });
+
+  describe('#fetchAll', function() {
+    it('returns all records in the table', function(done) {
+      var cache = Object.create(LocalResource)
+        .setSchemaBuilder(getSchemaBuilder())
+        .setTableName('my_table')
+        .setStoreType(lf.schema.DataStoreType.MEMORY)
+        .createTable();
+      var connection = cache.connectToDb();
+      cache.persist(connection, {}).then(function(records) {
+        var id = records[0].id;
+        cache.fetchAll(connection).then(function(records) {
+          if (records[0].id === id) {
+            connection.then(function(db) {
+              db.close();
+              done();
+            });
+          } else {
+            done.fail('record should be returned');
+          }
+        });
+      });
+    });
+  });
+});
