6562303223b3bb73addf6c929488501941c13bfe
Eric Carty-Fickes <ericcf@northwestern.edu>
2016-01-13 08:22:00 -0600

    add Ajax get function

--- src/Ajax.js
+++ src/Ajax.js
@@ -32,6 +32,38 @@
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(context.JSON.stringify(data));
       });
+    },
+
+    get: function get(url) {
+      return new Promise(function(resolve, reject) {
+        var request = new XMLHttpRequest();
+
+        request.onload = function onload() {
+          if (request.status === 200) {
+            try {
+              var responseObject = JSON.parse(request.responseText);
+              resolve(responseObject);
+            }
+            catch (error) {
+              reject(error);
+            }
+          } else {
+            reject(Error(request.statusText));
+          }
+        };
+
+        request.onerror = function onerror() {
+          reject(Error('Network Error'));
+        };
+
+        request.ontimeout = function ontimeout() {
+          reject(Error('Network Error'));
+        };
+
+        request.open('GET', url);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send();
+      });
     }
   };
 
--- test/AjaxSpec.js
+++ test/AjaxSpec.js
@@ -1,35 +1,43 @@
 'use strict';
 
 describe('Ajax', function() {
-  describe('.create', function() {
-    var url = 'https://api.example.com';
+  var url = 'https://api.example.com';
 
-    var TestResponses = {
-      create: {
-        bad_request: {
-          status: 400
-        },
-        valid_created: {
-          status: 201,
-          responseText: '{"data":[{"uuid":"abcd"}]}'
-        },
-        invalid_created: {
-          status: 201,
-          responseText: 'asdf'
-        }
+  var TestResponses = {
+    create: {
+      bad_request: {
+        status: 400
+      },
+      valid_created: {
+        status: 201,
+        responseText: '{"data":[{"uuid":"abcd"}]}'
+      },
+      invalid_created: {
+        status: 201,
+        responseText: 'asdf'
+      },
+      valid_fetched: {
+        status: 200,
+        responseText: '{"data":[{"uuid":"abcd"}]}'
+      },
+      invalid_fetched: {
+        status: 200,
+        responseText: 'asdf'
       }
-    };
+    }
+  };
 
-    beforeEach(function() {
-      jasmine.Ajax.install();
-      jasmine.clock().install();
-    });
+  beforeEach(function() {
+    jasmine.Ajax.install();
+    jasmine.clock().install();
+  });
 
-    afterEach(function() {
-      jasmine.clock().uninstall();
-      jasmine.Ajax.uninstall();
-    });
+  afterEach(function() {
+    jasmine.clock().uninstall();
+    jasmine.Ajax.uninstall();
+  });
 
+  describe('.post', function() {
     describe('when there is a network timeout', function() {
       it('rejects the promise', function(done) {
         cbit.Ajax.post(url).then(done.fail).catch(done);
@@ -74,6 +82,56 @@ describe('Ajax', function() {
 
           jasmine.Ajax.requests.mostRecent()
             .respondWith(TestResponses.create.invalid_created);
+        });
+      });
+    });
+  });
+
+  describe('.get', function() {
+    describe('when there is a network timeout', function() {
+      it('rejects the promise', function(done) {
+        cbit.Ajax.get(url).then(done.fail).catch(done);
+
+        jasmine.Ajax.requests.mostRecent().responseTimeout();
+      });
+    });
+
+    describe('when there is a network error', function() {
+      it('rejects the promise', function(done) {
+        cbit.Ajax.get(url).then(done.fail).catch(done);
+
+        jasmine.Ajax.requests.mostRecent().responseError();
+      });
+    });
+
+    describe('when there is a non-201 response status', function() {
+      it('rejects the promise', function(done) {
+        cbit.Ajax.get(url).then(done.fail).catch(done);
+
+        jasmine.Ajax.requests.mostRecent()
+          .respondWith(TestResponses.create.bad_request);
+      });
+    });
+
+    describe('when there is a 200 response status', function() {
+      describe('and the response body is valid JSON', function() {
+        it('resolves the promise with the deserialized JSON', function(done) {
+          cbit.Ajax.get(url).then(function(result) {
+            expect(result.data).toEqual([{ uuid: 'abcd' }]);
+            done();
+          }).catch(done.fail);
+
+          jasmine.Ajax.requests.mostRecent()
+            .respondWith(TestResponses.create.valid_fetched);
+        });
+      });
+
+      describe('and the response body is not valid JSON', function() {
+        it('rejects the promise', function(done) {
+          cbit.Ajax.get(url).then(done.fail).catch(done);
+
+          jasmine.Ajax.requests.mostRecent()
+            .respondWith(TestResponses.create.invalid_fetched);
         });
       });
     });
